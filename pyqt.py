84f0b824e86ed79005fc75feb3fb41b85eb239ed
masha <algabekovam26@sch57.ru>
2024-12-27 16:42:33 +0300

    Из кругов сделала окружности!

--- pyqt.py
+++ pyqt.py
@@ -2,7 +2,7 @@ import sys
 from random import randint
 from PyQt6 import uic
 from PyQt6.QtWidgets import QApplication, QMainWindow
-from PyQt6.QtGui import QPainter, QColor
+from PyQt6.QtGui import QPainter, QColor, QPen
 from PyQt6.QtCore import QPoint
 
 
@@ -25,9 +25,11 @@ class MyWidget(QMainWindow):
 
     def draw_circle(self, qp, event):
         if self.was_click:
-            qp.setBrush(QColor(255, 255, 0))
+            qp.setBrush(QColor(0, 0, 0, 0))
             r = randint(10, 200)
-            x, y = randint(100, 400), randint(100, 400)
+            pen = QPen(QColor("yellow"), 5)
+            qp.setPen(pen)
+            x, y = randint(100, 800), randint(100, 400)
             qp.drawEllipse(QPoint(x, y), r, r)
             self.was_click = False
 
