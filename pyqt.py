46df7d4534918f93bc343909312159566d3ab077
masha <algabekovam26@sch57.ru>
2024-12-27 16:58:54 +0300

    Разноцветные окружности, ui.py подключен к основному файлу

--- pyqt.py
+++ pyqt.py
@@ -1,15 +1,18 @@
 import sys
-from random import randint
-from UI.py import UI_MainWindow
+from random import randint, choice
+from ui import Ui_MainWindow
 from PyQt6.QtWidgets import QApplication, QMainWindow
 from PyQt6.QtGui import QPainter, QColor, QPen
 from PyQt6.QtCore import QPoint
 
 
-class MyWidget(QMainWindow):
+colors = ["red", "yellow", "green", "cyan", "blue", "purple", "black", "magenta"]
+
+
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.was_click = False
         self.pushButton.clicked.connect(self.change)
 
@@ -27,7 +30,7 @@ class MyWidget(QMainWindow):
         if self.was_click:
             qp.setBrush(QColor(0, 0, 0, 0))
             r = randint(10, 200)
-            pen = QPen(QColor("yellow"), 5)
+            pen = QPen(QColor(choice(colors)), 5)
             qp.setPen(pen)
             x, y = randint(100, 800), randint(100, 400)
             qp.drawEllipse(QPoint(x, y), r, r)
